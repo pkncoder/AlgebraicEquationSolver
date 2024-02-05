d0b753d13957b39ad002e2b03962f5b03c5b88d5
pkncoder <128310642+pkncoder@users.noreply.github.com>
2024-02-05 04:15:48 +0000

    Made a basic test file that is seperate

---
+++ NumeralExponentTestsFile.py
@@ -0,0 +1,9 @@
+# Import the exponent test
+from NumeralExponentTest import Numeral
+
+# Make the Numerals (and thank that I made initialization better)
+numOne = Numeral(3, "x", 4)
+numTwo = Numeral(7, "x", 4)
+
+# Print the new value of the two added together
+print(numOne + numTwo)
