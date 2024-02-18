9be3042828f66ed8e29183cdbc9216df1cccf41b
pkncoder <alex.h.preston@gmail.com>
2024-02-18 09:36:36 -0500

    Relized this was rong alot

--- solverTests/testSolver.py
+++
@@ -1,8 +0,0 @@
-from testSimplifier import simplify
-
-equation = input("Input your equation to simplify it: ")
-
-simpleEquation = simplify(equation)
-
-for numeral in simpleEquation:
-    print(numeral)
